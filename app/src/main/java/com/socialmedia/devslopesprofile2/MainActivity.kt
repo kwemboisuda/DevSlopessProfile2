0f1d13ca45318f829e5e03f4d4df3a3b83b5503e
kwemboSuda <kwemboisudaiz65@gmail.com>
2023-04-06 11:06:56 +0300

    Intens and Context Concepts

--- app/src/main/java/com/socialmedia/devslopesprofile2/MainActivity.kt
+++ app/src/main/java/com/socialmedia/devslopesprofile2/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.socialmedia.devslopesprofile2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val getStartedBtn=findViewById<Button>(R.id.getStartedBtn)
+        getStartedBtn.setOnClickListener{
+            val leagueActivityIntent= Intent(this,LeagueActivity::class.java)
+            startActivity(leagueActivityIntent)
+        }
+
+
     }
 }
