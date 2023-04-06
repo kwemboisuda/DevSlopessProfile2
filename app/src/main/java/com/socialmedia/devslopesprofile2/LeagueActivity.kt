ff3c489f8f630fc4180bd2ec9dad736bab8bf065
kwemboSuda <kwemboisudaiz65@gmail.com>
2023-04-06 10:30:48 +0300

    Select button changes

--- app/src/main/java/com/socialmedia/devslopesprofile2/LeagueActivity.kt
+++ app/src/main/java/com/socialmedia/devslopesprofile2/LeagueActivity.kt
@@ -2,10 +2,15 @@ package com.socialmedia.devslopesprofile2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        val btnNext= findViewById<TextView>(R.id.button4)
+
     }
+
 }
